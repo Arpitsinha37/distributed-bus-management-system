7283e28a89c9329934e0ad3177088bfd468a3ec9
Arpit Sinha <arpitsinha579@gmail.com>
2026-09-02 11:39:57 +0530

    Revert incorrectly modified DTO imports

--- bus-booking-backend/src/staff/dto/update-staff.dto.ts
+++ bus-booking-backend/src/staff/dto/update-staff.dto.ts
@@ -1,5 +1,5 @@
 import { IsArray, IsBoolean, IsEmail, IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
-import { StaffRole } from '../common/enums/roles.enum';
+import { StaffRole } from '../../common/enums/roles.enum';
 
 export class UpdateStaffDto {
   @IsOptional() @IsString()
